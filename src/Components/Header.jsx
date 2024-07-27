52d9e28cfa9624d8f6eab21e89d552c75fe5631f
Abhishek Nandan <135464165+kronocodes@users.noreply.github.com>
2024-07-27 20:50:05 +0530

    ok

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
 
   return (
     <>
-      <div className="flex justify-between max-w-[1300px] mx-auto py-3 px-3 font-medium">
+      <div className="flex justify-between max-w-[1200px] mx-auto py-3 px-3 font-medium">
         <div className="font-bold">ProjectX</div>
         <div className="flex gap-x-10">
           <Link>OA Practice</Link>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import About from "./Components/About/About";
+import About from "./Components/About";
 import Home from "./Components/Home";
 import Contact from "./Components/Contact";
 import Work from "./Components/Work";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
-import Error from "./Components/Others/Error";
-import ScrollToTop from "./Components/Others/ScrollToTop";
+import Error from "./Components/Error";
+import ScrollToTop from "./Components/ScrollToTop";
 
 
 const router = createBrowserRouter([
