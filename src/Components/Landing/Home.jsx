65c81b01ccc0fd4f275e38c895d20bcf4712237e
kronocodes <abhisheknandan2013@gmail.com>
2024-07-31 14:43:49 +0530

    clean

---
+++ src/Components/Landing/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './Hero'
+import { Domains } from '../Screen/Landing Screen/Domains'
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <Domains/>
+      
+      </>
+  )
+}
+
+export default Home
